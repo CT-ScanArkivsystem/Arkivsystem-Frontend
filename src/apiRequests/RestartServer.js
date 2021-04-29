b146973fd7d6582fe60f08ffcb74bf9f22b9c1da
Brage Tranvik <brage@tranvik.no>
2021-04-29 11:27:11 +0200

    Fixed some css

--- src/apiRequests/RestartServer.js
+++ src/apiRequests/RestartServer.js
@@ -1,7 +1,7 @@
 import {onError} from "../libs/errorLib";
 import {currentIP} from "../App";
 
-export default async function RestartServer(date, time, zone) {
+export default async function RestartServer(restartDate, restartTime, restartZone) {
     try {
         let res = await fetch(currentIP + '/admin/restartServer', {
             method: 'POST',
@@ -11,14 +11,14 @@ export default async function RestartServer(date, time, zone) {
             },
             body: JSON.stringify(
                 {
-                    date: date,
-                    time: time,
-                    zone: zone
+                    date: restartDate,
+                    time: restartTime,
+                    zone: restartZone
                 }
             )
         });
         if (res.ok) {
-            console.log("api: Set restart")
+            // console.log("api: Set restart")
         } else {
             console.log("api: Restart not set");
         }
